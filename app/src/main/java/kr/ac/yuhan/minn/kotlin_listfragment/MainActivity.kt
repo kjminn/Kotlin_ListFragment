28b47f8b15890ba818a4758c0a9b004a8aae1cba
kjminn <kjminn2001@gmail.com>
2022-11-22 16:51:12 +0900

    Activity에 Fragment 추가2

--- app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/MainActivity.kt
+++ app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setFragment()
+    }
+
+    fun setFragment(){
+        /*val listFragment:ListFragment = ListFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragmentLayout, listFragment)
+        transaction.commit()*/
     }
 }
