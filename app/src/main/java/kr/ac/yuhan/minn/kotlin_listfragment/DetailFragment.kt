0836b6dce8757a843b2ef0792e5d4e4c20c241ad
kjminn <kjminn2001@gmail.com>
2022-11-23 13:54:53 +0900

    Fragment Next와 Go 버튼

--- app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/DetailFragment.kt
+++ app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/DetailFragment.kt
@@ -6,26 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.ac.yuhan.minn.kotlin_listfragment.databinding.FragmentListBinding
+import kr.ac.yuhan.minn.kotlin_listfragment.databinding.FragmentDetailBinding
 
+class DetailFragment : Fragment() {
+    lateinit var mainActivity:MainActivity
 
-class ListFragment : Fragment() {
-    var minActivity: MainActivity? = null
-
-     override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentListBinding.inflate(inflater, container, false)
-        binding.btnNext.setOnClickListener {
-            minActivity?.goDetail()
+       val binding = FragmentDetailBinding.inflate(inflater, container, false)
+        binding.btnBack.setOnClickListener {
+            mainActivity.goBack()
         }
-
         return binding.root
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is MainActivity) minActivity = context
+        mainActivity = context as MainActivity
     }
 }
--- app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/MainActivity.kt
+++ app/src/main/java/kr/ac/yuhan/minn/kotlin_listfragment/MainActivity.kt
@@ -7,13 +7,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setFragment()
+
     }
 
     fun setFragment(){
-        /*val listFragment:ListFragment = ListFragment()
+        val listFragment = ListFragment()
+
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragmentLayout, listFragment)
-        transaction.commit()*/
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
     }
+
+    fun goDetail(){
+        val detailFragment = DetailFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, detailFragment)
+        transaction.addToBackStack("detail")
+        transaction.commit()
+    }
+
+    fun goBack(){
+        onBackPressed()
+    }
+
 }
